905320104e6dad0e0db79c826badf7987cc4cee0
leyseth <sloef202@hotmail.com>
2018-09-06 15:40:57 +0200

    Cleaned vers
    Cleaned version

--- mysite/movies/urls.py
+++ mysite/movies/urls.py
@@ -5,7 +5,4 @@ app_name = 'quotes'
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	url(r'search/quotes', views.search_quotes, name='search_quotes'),
-	url(r'search/form', views.search_form, name='search_form'),
-	url(r'^(?P<author_name>[A-z ]+)/$', views.detail, name='detail'),
 ]
--- mysite/movies/views.py
+++ mysite/movies/views.py
@@ -1,27 +1,12 @@
 from django.shortcuts import render
 
-from .models import Author
-from .models import Quote
+import redis
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+
 import string
 
 def index(request):
-    author_names = [a.author_name for a in Author.objects.all()]
+    for key in r.scan_iter("user:*"):
+        output = r.get(key)
 
-    return render(request, 'quotes/index.html', {'author_names': author_names})
-
-def detail(request, author_name):
-    author = Author.objects.filter(author_name=author_name).first()
-    quote_list = author.quote_set.all()
-
-    return render(request, 'quotes/detail.html', {'quote_list': quote_list})
-
-def search_form(request):
-    return render(request, 'quotes/search_form.html', {})
-
-def search_quotes(request):
-    if request.method == 'POST':
-        word = request.POST['search_term']
-        quote_list = Quote.objects.all()
-        result = [q.quote_text for q in quote_list if q.search_quote(word)]
-
-    return render(request, 'quotes/detail.html', {'quote_list': result})
+    return render(request, 'movies/index.html')
